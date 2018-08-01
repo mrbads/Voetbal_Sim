5b508ec0ad8e8c62cc0ffd56e648881f29bad933
Bas van der Borden <bascasper@gmail.com>
2018-08-01 17:04:52 +0200

    main class and quick_match added

--- main.py
+++ main.py
@@ -19,54 +19,67 @@ leagues = [
 ['Telstar', 60],['TOP Oss', 60]]]
 ]
 
-def welcome():
-    print(" __      __        _   _           _    _____ _")
-    print(" \ \    / /       | | | |         | |  / ____(_)")
-    print("  \ \  / /__   ___| |_| |__   __ _| | | (___  _ _ __ ___")
-    print("   \ \/ / _ \ / _ \ __| '_ \ / _` | |  \___ \| | '_ ` _ \ ")
-    print("    \  / (_) |  __/ |_| |_) | (_| | |  ____) | | | | | | |")
-    print("     \/ \___/ \___|\__|_.__/ \__,_|_| |_____/|_|_| |_| |_|")
-    print("")
-    print('welcome to Voetbal Sim')
+class Main(object):
+    """docstring for Main."""
+    def __init__(self):
+        super(Main, self).__init__()
+        self.main()
 
-def menu():
-    print('[1] New Game \n[2] Career mode \n[3] Info \n[4] Quit')
-    option = int(input('your choice: '))
-    if option == 1:
-        print('New Game')
-        teams = selection()
-        return(teams)
-    elif option == 2:
-        print('Career mode')
-        manager = Manager('Ajax', clubs, leagues[0])
-        manager.menu()
-    elif option == 3:
-        print('Info')
-        print('Voetbal_Sim is a football simulator based upon fifa career mode.')
-        menu()
-    else:
-        exit()
+    def welcome(self):
+        print(" __      __        _   _           _    _____ _")
+        print(" \ \    / /       | | | |         | |  / ____(_)")
+        print("  \ \  / /__   ___| |_| |__   __ _| | | (___  _ _ __ ___")
+        print("   \ \/ / _ \ / _ \ __| '_ \ / _` | |  \___ \| | '_ ` _ \ ")
+        print("    \  / (_) |  __/ |_| |_) | (_| | |  ____) | | | | | | |")
+        print("     \/ \___/ \___|\__|_.__/ \__,_|_| |_____/|_|_| |_| |_|")
+        print("")
+        print('welcome to Voetbal Sim')
 
-def selection():
-    print('Choose your team:')
-    for team in range(0,len(clubs)):
-        print('[{}] - {}'.format(team, clubs[team]))
-    option = int(input())
-    home = club.Club(clubs[option])
-    print(home.name)
-    print('Choose your opponent:')
-    for team in range(0,len(clubs)):
-        print('[{}] - {}'.format(team, clubs[team]))
-    option = int(input())
-    away = club.Club(clubs[option])
-    print(away.name)
-    return(home, away)
+    def menu(self):
+        print('[1] New Game \n[2] Career mode \n[3] Info \n[4] Quit')
+        option = int(input('your choice: '))
+        if option == 1:
+            print('New Game')
+            teams = self.selection()
+            self.quick_match(teams)
+            self.menu()
+        elif option == 2:
+            print('Career mode')
+            manager = Manager('Ajax', clubs, leagues[0])
+            manager.menu()
+        elif option == 3:
+            print('Info')
+            print('Voetbal_Sim is a football simulator based upon fifa career mode.')
+            menu()
+        else:
+            exit()
 
-def main():
-    welcome()
-    teams = menu()
-    Simulation.simulate(teams)
-    pass
+    def selection(self):
+        print('Choose your team:')
+        for team in range(0,len(clubs)):
+            print('[{}] - {}'.format(team, clubs[team]))
+        option = int(input())
+        home = club.Club(clubs[option])
+        print(home.name)
+        print('Choose your opponent:')
+        for team in range(0,len(clubs)):
+            print('[{}] - {}'.format(team, clubs[team]))
+        option = int(input())
+        away = club.Club(clubs[option])
+        print(away.name)
+        return(home, away)
+
+    def main(self):
+        self.welcome()
+        self.menu()
+
+    def quick_match(self, teams):
+        home = teams[0]
+        away = teams[1]
+        print('Selected teams: \n {} vs {}'.format(home.name, away.name))
+        input('press enter to continue')
+        result = Simulation.simulate(teams)
+        print(' {} vs {}\n {} - {}'.format(result[0].name, result[1].name, result[0].result, result[1].result))
 
 if __name__ == '__main__':
-    main()
+    Main()
